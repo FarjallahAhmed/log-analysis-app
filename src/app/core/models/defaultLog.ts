b93daf4cfea9e0586a42ac96ea52b08423e043ca
FarjallahAhmed <ahmed.farjallah@esprit.tn>
2023-03-21 14:17:51 +0100

    Create the Default models

---
+++ src/app/core/models/defaultLog.ts
@@ -0,0 +1,16 @@
+export class DefaultLog{
+
+    logLevel!: string;
+    logMessage!: string;
+    logger!: string;
+    year!: number;
+    month!: number;
+    day!: number;
+    package!: string;
+    uuid!: string;
+    thread!: string;
+    logDate!: Date;
+    login!: string;
+    pwd!:  string;
+
+}
