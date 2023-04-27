d45218aeed32b7a621f79d913f720995b3f9c427
FarjallahAhmed <ahmed.farjallah@esprit.tn>
2023-04-27 17:22:16 +0100

    implements the summary service and test it in the html component

--- src/app/dashboard/components/summary/summary.component.ts
+++ src/app/dashboard/components/summary/summary.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SummaryService } from '../../services/summary.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss']
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
+
+  response!: any;
+  summaryData$!: any;
+
+
+  constructor(private summaryService: SummaryService){}
+
+  ngOnInit(): void {
+    this.summaryService.getSummaryOfLogs().subscribe(data => {
+
+      this.response = data;
+      console.log(this.response);
+
+    });
+  }
 
 }
--- src/app/dashboard/services/summary.service.ts
+++ src/app/dashboard/services/summary.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class SummaryService {
 
-  constructor() { }
+  private baseUrl= "http://localhost:8080/agg/";
+
+  constructor(private http: HttpClient) { }
+
+
+  getSummaryOfLogs(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}`+'summary').pipe(
+      map(response => {
+        const logData = {
+          totalLogs: response.totalLogs,
+          stackTraceLogs: response.stackTraceLogs,
+          latestDate: response.latestDate,
+          earliestDate: response.earliestDate,
+          errorLogs: response.errorLogs,
+          topLoggers: response.topLoggers,
+          logLevelPercentages: new Map<string,number>(),
+          errorMessagePercentages: new Map<string,number>()
+        };
+        Object.keys(response.errorMessagePercentages).forEach(key => {
+          logData.errorMessagePercentages.set(key, Math.round(response.errorMessagePercentages[key] * 100) / 100);
+        });
+        Object.keys(response.logLevelPercentages).forEach(key => {
+          logData.logLevelPercentages.set(key,  Math.round(response.logLevelPercentages[key] * 100) / 100);
+        });
+
+        return logData;
+      })
+    );
+  }
 }
+
+
+
