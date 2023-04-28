34dfc0ea1187e817c706a9ca9d6f18296a3590c4
FarjallahAhmed <ahmed.farjallah@esprit.tn>
2023-04-28 10:39:52 +0100

    implements filterLogsWithDateRange to make the pissibility to return the logs in a range of time

--- src/app/dashboard/components/filter/filter.component.ts
+++ src/app/dashboard/components/filter/filter.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilterService } from '../../services/filter.service';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit{
+
+  logs!: any[];
+  constructor(private filterService: FilterService){}
+
+  ngOnInit(): void {
+    const startDate = new Date('2022-01-01');
+    const endDate = new Date('2023-01-31');
+    this.filterService.filterLogsWithDateRange(startDate,endDate).subscribe(
+      data =>{
+        this.logs = data;
+      }
+    )
+  }
 
 }
+
+
+
--- src/app/dashboard/services/filter.service.ts
+++ src/app/dashboard/services/filter.service.ts
@@ -1,9 +1,56 @@
+import { formatDate } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/logs/filter/';
+
+
+  constructor(private http: HttpClient) { }
+
+
+  filterLogsWithDateRange(startDate: Date, endDate: Date): Observable<any> {
+    const params = {
+      'start-date': startDate.toISOString().slice(0, 10),
+      'end-date': endDate.toISOString().slice(0, 10)
+    };
+
+    return this.http.get<any>(`${this.apiUrl}` + 'logs-by-date-range', { params }).pipe(
+      //map(response => response.logs)
+      map(response => {
+        return response.logs.map((log: {
+          log_date: string;
+          loglevel: any;
+          logger: any;
+          threadname: any;
+          logmessage: any;
+          uuid: any;
+          package: any;
+        }) => {
+          const logDate = log.log_date;
+          const logLevel = log.loglevel;
+          const logger = log.logger;
+          const threadName = log.threadname;
+          const logmessage = log.logmessage;
+          const uuid = log.uuid;
+          const packagee = log.package;
+          return {
+            packagee,
+            logger,
+            logmessage: logmessage,
+            uuid,
+            loglevel: logLevel,
+            threadname: threadName,
+            log_date: logDate
+          };
+        });
+      })
+    );
+  }
 }
